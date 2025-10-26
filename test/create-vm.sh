f1dd24df6442fbfb707354925fe532e250fca228
Loan RIYANTO <loan.riyanto@epitech.eu>
2025-10-26 18:41:31 +0100

    fix: fixed network connectivity problem by using standard interface, and fixed qcow2 image problem

--- test/create-vm.sh
+++ test/create-vm.sh
@@ -1,21 +1,22 @@
 VM_NAME=ubuntu-cloud-vm
 CLOUD_IMG_URL=https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-CLOUD_IMG_FILE="${CLOUD_IMG_URL##*/}"
-CLOUD_IMG_QCOW="${CLOUD_IMG_FILE%.*}.qcow2"
+CLOUD_IMG_QCOW="os.qcow2"
 IMG_PATH=/var/lib/libvirt/images/
 
 virsh destroy $VM_NAME
 virsh undefine $VM_NAME --remove-all-storage
 rm -f "${IMG_PATH}${CLOUD_IMG_QCOW}"
+rm -f "${IMG_PATH}seed.iso"
 
 ls /var/lib/libvirt/images
 # download cloud image
 wget -nc -P $IMG_PATH $CLOUD_IMG_URL
 
-qemu-img convert -f raw -O qcow2 "${IMG_PATH}${CLOUD_IMG_FILE}" "${IMG_PATH}${CLOUD_IMG_QCOW}"
+cp "${IMG_PATH}${CLOUD_IMG_URL##*/}" "${IMG_PATH}${CLOUD_IMG_QCOW}"
 
-chown libvirt-qemu:kvm /var/lib/libvirt/images/jammy-server-cloudimg-amd64.qcow2
-chmod 0640 /var/lib/libvirt/images/jammy-server-cloudimg-amd64.qcow2
+
+chown libvirt-qemu:kvm /var/lib/libvirt/images/os.qcow2
+chmod 0640 /var/lib/libvirt/images/os.qcow2
 
 
 #generate configuration iso for cloud image, see what's in seed-config/
