eacfc47784e2da525cd3d8f0b1ac4cf63c417297
skl1017 <loan.riyanto.pro@gmail.com>
2025-10-28 20:41:24 +0100

    feat: added libvirt dependencies for python in libvirt-install.sh

--- test/libvirt-install.sh
+++ test/libvirt-install.sh
@@ -1,4 +1,4 @@
 echo "Installing libvirt dependencies..."
-sudo apt install qemu-kvm libvirt-daemon-system genisoimage libvirt-clients bridge-utils virtinst
+sudo apt install qemu-kvm libvirt-daemon-system genisoimage libvirt-clients bridge-utils virtinst pkg-config libvirt-dev python3-dev
 echo "Enabling libvirt daemon (libvirtd)..."
 sudo systemctl enable libvirtd
