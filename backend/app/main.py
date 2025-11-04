8d92159679b510d06ffe9b667b757290fbcef8d3
skl1017 <loan.riyanto.pro@gmail.com>
2025-11-04 10:11:08 +0100

    feat: added 2 routes, one for vm creation and another for vm start

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,31 @@
 from typing import Union
+from fastapi import FastAPI, HTTPException, status
 
-from fastapi import FastAPI
+from app.models.vm import VmCreate
+from app.services.vm_service import VmService
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/vms/{vm_id}/start")
+def read_root(vm_id: str):
+    try:
+        VmService.start_vm(vm_id)
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+    )
+    # returns the started vm 
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/vms", status_code=status.HTTP_201_CREATED)
+def create_vm(vm:VmCreate):
+    try:
+        created_vm = VmService.create_vm(vm)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(e)
+        )
+    return created_vm
