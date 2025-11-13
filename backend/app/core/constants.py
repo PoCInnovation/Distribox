4d257fd1fff9cb77838243b39e0e49d504e8d798
lg-epitech <146029080+lg-epitech@users.noreply.github.com>
2025-11-13 13:37:02 +0100

    fix: Change VM_STATE_NAMES for blocked state to English

--- backend/app/core/constants.py
+++ backend/app/core/constants.py
@@ -8,7 +8,7 @@ IMAGES_DIR = BASE_DIR / 'images'
 VM_STATE_NAMES = {
     libvirt.VIR_DOMAIN_NOSTATE: "No state",
     libvirt.VIR_DOMAIN_RUNNING: "Running",
-    libvirt.VIR_DOMAIN_BLOCKED: "Bloqué sur E/S",
+    libvirt.VIR_DOMAIN_BLOCKED: "Blocked on I/O",
     libvirt.VIR_DOMAIN_PAUSED: "Paused",
     libvirt.VIR_DOMAIN_SHUTDOWN: "Stopping",
     libvirt.VIR_DOMAIN_SHUTOFF: "Stopped",
