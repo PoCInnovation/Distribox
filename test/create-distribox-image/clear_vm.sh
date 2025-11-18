94198842c2f2808ad0cff67f04c7cbbada44df29
skl1017 <loan.riyanto.pro@gmail.com>
2025-11-18 17:36:25 +0100

    fix: changed vm creation script for distribox image, now using virt-customize to install dependencies and not cloud-init anymore. cloud-init will only be used on the user side

--- test/create-distribox-image/clear_vm.sh
+++ test/create-distribox-image/clear_vm.sh
@@ -1,5 +1,23 @@
-##on host machine 
-virt-sysprep -a /var/lib/distribox/images/os.qcow2   --operations bash-history,logfiles,tmp-files,crash-data,utmp
-virt-sparsify --compress /var/lib/distribox/images/os.qcow2 /var/lib/distribox/images/distribox-ubuntu.qcow2
-rm -f /var/lib/distribox/images/os.qcow2 
-rm -f /var/lib/distribox/images/seed.iso
+#!/bin/bash
+##on host machine
+#execute using sudo
+
+set -e 
+sudo qemu-img create -f qcow2 /tmp/resized_image.qcow2 10G
+
+sudo virt-resize --expand /dev/sda1 \
+    /var/lib/distribox/images/os.qcow2 \
+    /tmp/resized_image.qcow2
+
+sudo virt-customize -a /tmp/resized_image.qcow2 \
+    --network \
+    --update \
+    --install vim,qemu-guest-agent,cloud-init,linux-virtual-hwe-22.04,xubuntu-desktop \
+    --run-command 'update-initramfs -u' \
+    --run-command 'update-grub' \
+    --run-command 'grub-install /dev/sda'
+
+sudo virt-sysprep -a /tmp/resized_image.qcow2 --operations machine-id,ssh-hostkeys
+
+sudo virt-sparsify --compress /tmp/resized_image.qcow2 /var/lib/distribox/images/distribox-ubuntu.qcow2
+rm -f /tmp/resized_image.qcow2
--- test/create-distribox-image/create-distribox-image.sh
+++ test/create-distribox-image/create-distribox-image.sh
@@ -7,21 +7,22 @@ DISTRIBOX_IMG_PATH=/var/lib/distribox/images/
 virsh destroy $VM_NAME
 virsh undefine $VM_NAME --remove-all-storage
 rm -f "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}"
-rm -f "${DISTRIBOX_IMG_PATH}seed.iso"
+# rm -f "${DISTRIBOX_IMG_PATH}seed.iso"
 
 # download cloud image
 wget -nc -P $LIBVIRT_IMG_PATH $CLOUD_IMG_URL
 
 cp "${LIBVIRT_IMG_PATH}${CLOUD_IMG_URL##*/}" "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}"
 
-qemu-img resize "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}" +5G
+# qemu-img resize "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}" +9G
 
 
 #generate configuration iso for cloud image, see what's in seed-config/
-genisoimage -output "${DISTRIBOX_IMG_PATH}seed.iso" -volid cidata -joliet -rock seed-config/
+# genisoimage -output "${DISTRIBOX_IMG_PATH}seed.iso" -volid cidata -joliet -rock seed-config/
+# genisoimage -output "/var/lib/distribox/images/seed.iso" -volid cidata -joliet -rock seed-config/
 
-#defines and starts vm using vm.xml
-virsh define vm.xml
-virsh start $VM_NAME
+# #defines and starts vm using vm.xml
+# virsh define vm.xml
+# virsh start $VM_NAME
 
 ##run clear_vm.sh after the vm creation. It will shutdown when init is finished
