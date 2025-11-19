22181bca5af5a667c9b7b907d6eaa687b7be0f33
skl1017 <loan.riyanto.pro@gmail.com>
2025-11-19 19:42:16 +0100

    fix: changed virtual image size to 5G instead of 10

--- test/create-distribox-image/create-distribox-image.sh
+++ test/create-distribox-image/create-distribox-image.sh
@@ -1,28 +1,36 @@
-VM_NAME=ubuntu-cloud-vm
+#!/bin/bash
+
 CLOUD_IMG_URL=https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-CLOUD_IMG_QCOW="os.qcow2"
-LIBVIRT_IMG_PATH="/tmp/"
-DISTRIBOX_IMG_PATH=/var/lib/distribox/images/
 
-virsh destroy $VM_NAME
-virsh undefine $VM_NAME --remove-all-storage
-rm -f "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}"
-# rm -f "${DISTRIBOX_IMG_PATH}seed.iso"
+set -e 
+sudo qemu-img create -f qcow2 /tmp/resized_image.qcow2 5G
+sudo virt-resize --expand /dev/sda1 \
+    /var/lib/distribox/images/os.qcow2 \
+    /tmp/resized_image.qcow2
 
-# download cloud image
-wget -nc -P $LIBVIRT_IMG_PATH $CLOUD_IMG_URL
+sudo virt-customize -a /tmp/resized_image.qcow2 \
+    --network \
+    --update \
+    --install vim,qemu-guest-agent,cloud-init,linux-virtual-hwe-22.04,xubuntu-desktop \
+    --run-command 'update-initramfs -u' \
+    --run-command 'update-grub' \
+    --run-command 'grub-install /dev/sda'
 
-cp "${LIBVIRT_IMG_PATH}${CLOUD_IMG_URL##*/}" "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}"
+# 3. Nettoyage
+sudo virt-sysprep -a /tmp/resized_image.qcow2 --operations machine-id,ssh-hostkeys
 
-# qemu-img resize "${DISTRIBOX_IMG_PATH}${CLOUD_IMG_QCOW}" +9G
+# 4. Réduction de la partition (sda3: 10G -> 4.5G)
+# Crée un nouveau fichier intermédiaire pour la réduction
 
+sudo virt-sparsify --compress /tmp/resized_image.qcow2 \
+    /var/lib/distribox/images/distribox-ubuntu.qcow2
 
-#generate configuration iso for cloud image, see what's in seed-config/
-# genisoimage -output "${DISTRIBOX_IMG_PATH}seed.iso" -volid cidata -joliet -rock seed-config/
-# genisoimage -output "/var/lib/distribox/images/seed.iso" -volid cidata -joliet -rock seed-config/
+# 5. Réduction de la taille virtuelle (4.5G -> 5G)
+# Réduit la taille maximale du disque (VirtIO)
+# sudo qemu-img resize /tmp/shrunk_partition.qcow2 5G
 
-# #defines and starts vm using vm.xml
-# virsh define vm.xml
-# virsh start $VM_NAME
+# 6. Finalisation (Sparsification et Compression)
+# Crée le template final optimisé
 
-##run clear_vm.sh after the vm creation. It will shutdown when init is finished
+# 7. Nettoyage des fichiers temporaires
+sudo rm -f /tmp/resized_image.qcow2
