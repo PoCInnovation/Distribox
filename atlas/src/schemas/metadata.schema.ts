e1707dabd5de933d2ce5885777a4c6093681e763
lg-epitech <laurent.gonzalez@epitech.eu>
2026-02-10 22:23:08 +0100

    fix: metadata schema

--- atlas/src/schemas/metadata.schema.ts
+++ atlas/src/schemas/metadata.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const MetadataSchema = z.object({
   name: z.string(),
   image: z.string(),
-  version: z.string(),
+  version: z.union([z.string(), z.number()]),
   distribution: z.string(),
   family: z.string(),
   revision: z.number(),
