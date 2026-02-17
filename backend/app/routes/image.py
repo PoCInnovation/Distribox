e881a78f87e816857d0ce794e28d6088f61f9a3c
lg-epitech <laurent.gonzalez@epitech.eu>
2026-02-17 23:11:16 +0100

    feat: update all routes to adhere to policies

--- backend/app/routes/image.py
+++ backend/app/routes/image.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 from app.services.image_service import ImageService
 from app.models.image import ImageRead
+from app.models.user_management import MissingPoliciesResponse
+from app.utils.auth import require_policy
 
 
 router = APIRouter()
 
 
 @router.get("/", status_code=status.HTTP_200_OK,
-            response_model=list[ImageRead])
+            response_model=list[ImageRead],
+            dependencies=[Depends(require_policy("images:get"))],
+            responses={403: {"model": MissingPoliciesResponse}})
 def get_distribox_image_list():
     try:
         return ImageService.get_distribox_image_list()
