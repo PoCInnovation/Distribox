12ae57dec1cb6dc1e70c271c222672d60fca18bf
Laurent Gonzalez <laurent.gonzalez@epitech.eu>
2026-03-29 13:27:42 +0200

    fix(frontend): can't connect to vm while suspended

--- frontend/app/components/dashboard/vm-details-dialog.tsx
+++ frontend/app/components/dashboard/vm-details-dialog.tsx
@@ -351,7 +351,9 @@ export function VMDetailsDialog({
           <Button
             variant="outline"
             disabled={
-              vm.state !== VMState.RUNNING || missingForConnect.length > 0
+              (vm.state !== VMState.RUNNING &&
+                vm.state !== VMState.PMSUSPENDED) ||
+              missingForConnect.length > 0
             }
             onClick={(e) => onConnectVM?.(vm, e)}
           >
